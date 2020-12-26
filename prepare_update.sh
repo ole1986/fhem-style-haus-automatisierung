4b51235106929f32077e83937c4c87fb0cad5448
ole1986 <ole.koeckemann@gmail.com>
2020-12-26 11:30:55 +0100

    Responsive design

--- prepare_update.sh
+++ prepare_update.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-#compass compile --force
-
 rm controls_ha_theme.txt
 
 echo "MOV ./www/hausautomatisierung-com/custom.js unused" >> controls_ha_theme.txt
 
-find ./www -type f \( ! -iname ".*" \) -print0 | while IFS= read -r -d '' f;
+find ./www -type f \( ! -iname ".*" \) -print0 | while IFS= read -r -d '' file;
 do
-    out="UPD "$(stat -f "%Sm" -t "%Y-%m-%d_%T" $f)" "$(stat -f%z $f)" ${f}"
-    echo ${out//.\//} >> controls_ha_theme.txt
+    date=$(stat -c '%y' $file)
+    size=$(stat -c '%s' $file)
+    echo  "UPD $date $size $file" >> controls_ha_theme.txt
 done
 
-# CHANGED file
-echo "FHEM haus-automatisierung.com custom theme last changes:" > CHANGED
-echo $(date +"%Y-%m-%d") >> CHANGED
-git log -n 10 --reverse --pretty="format:- %s" >> CHANGED
+# Append to changelog
+changes=$(git log -n 10 --reverse --pretty="format:- %s")
+echo -e "$(date +'%Y-%m-%d')\n$changes\n$(cat CHANGELOG)" > CHANGELOG
