74e6b7024014d7afea4fc9bdb611a3485929b34c
ole1986 <ole.koeckemann@gmail.com>
2020-12-26 12:44:44 +0100

    Updated contribute.s script

--- contribute.sh
+++ contribute.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FHEM_DIR=/opt/fhem/
+FHEM_DIR=/opt/fhem
 
 if [ "$1" == "clean" ]
 then
@@ -14,10 +14,12 @@ then
     exit
 fi
 
-[ ! -d $FHEM_DIR/www/hausautomatisierung-com ] && sudo ln -s $PWD/www/hausautomatisierung-com $FHEM_DIR/www/hausautomatisierung-com
-[ ! -d $FHEM_DIR/www/images/hausautomatisierung_com ] && sudo ln -s $PWD/www/images/hausautomatisierung_com $FHEM_DIR/www/images/hausautomatisierung_com
+
+
+[ ! -L $FHEM_DIR/www/hausautomatisierung-com ] && sudo rm -rf $FHEM_DIR/www/hausautomatisierung-com && sudo ln -s $PWD/www/hausautomatisierung-com $FHEM_DIR/www/hausautomatisierung-com
+[ ! -L $FHEM_DIR/www/images/hausautomatisierung_com ] && sudo rm -rf $FHEM_DIR/www/images/hausautomatisierung_com && sudo ln -s $PWD/www/images/hausautomatisierung_com $FHEM_DIR/www/images/hausautomatisierung_com
 
 for s in $PWD/www/pgm2/hausautomatisierung_com*
 do
-    [ ! -f $FHEM_DIR/www/pgm2/$(basename $s) ] && sudo ln -s $s $FHEM_DIR/www/pgm2/$(basename $s)
+    [ ! -L $FHEM_DIR/www/pgm2/$(basename $s) ] && sudo rm -f $FHEM_DIR/www/pgm2/$(basename $s) && sudo ln -s $s $FHEM_DIR/www/pgm2/$(basename $s)
 done
